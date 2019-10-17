dd6da86855ac8b083c20252e8099531afa4cf459
Lytton Liao <lytton.liao@gmail.com>
2019-10-17 15:53:52 -0400

    cleaned code

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,12 @@ const path = require('path');
 
 module.exports = {
     context: __dirname,
-    entry: './src/ticketapp_part3.js',
+    entry: './src/ticketapp.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.js', '.jsx', '*']
-    },
-    resolve: {
         extensions: ['.js']
     },
-    devtool: 'source-map'
 };
